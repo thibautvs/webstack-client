502af56904875c2eecd3f8337beb5df37f66c6d3
unknown <tvanspaandonck@ND1006-01.voo.intra>
2014-07-28 15:11:16 +0200

    Cloned AngularSeed

--- App/js/controllers.js
+++ App/js/controllers.js
@@ -1,15 +1,11 @@
-var productApp = angular.module('ProductsApp.controllers', []);
+'use strict';
 
-productApp.controller('ProductController', function ($scope) {
-  $scope.title = 'Product catalog';
-  $scope.nameFilter = null;
-  $scope.products = [
-    {'name': 'PS4', 'desc': 'New Sony console.'},
-    {'name': 'Air drone', 'desc': 'A remote-controlled helicopter.'},
-    {'name': 'Digital frame', 'desc': 'Your best memories, slide after slide.'}
-  ];
-  $scope.searchFilter = function (product) {
-    var keyword = new RegExp($scope.nameFilter, 'i');
-    return !$scope.nameFilter || keyword.test(product.name);
-  };
-});
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+  .controller('MyCtrl1', ['$scope', function($scope) {
+
+  }])
+  .controller('MyCtrl2', ['$scope', function($scope) {
+
+  }]);
