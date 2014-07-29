0275fbc37728154f5d13132a83c50848d7a29cb7
unknown <tvanspaandonck@ND1006-01.voo.intra>
2014-07-29 11:26:41 +0200

    Renamed views and controllers to Home and About

--- App/js/controllers.js
+++ App/js/controllers.js
@@ -3,9 +3,9 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('HomeController', ['$scope', function($scope) {
 
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+  .controller('AboutController', ['$scope', function($scope) {
 
   }]);
