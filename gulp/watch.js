fd243a92a46332cb791f33d1130eb691931473d6
voothib <thibaut.van.Spaandonck@staff.voo.be>
2014-08-04 14:12:51 +0200

    Gulp : added watcher task for JS files

--- gulp/watch.js
+++ gulp/watch.js
@@ -5,11 +5,18 @@ var config = require('./config');
 
 // Watch the files in the paths object, and when there is a change, run the functions in the array
 gulp.task('watch', function () {
-  gulp.watch(config.paths.css.files, ['sass'])
-    // Also when there is a change, display what file was changed, only showing the path after the 'css' folder
-    .on('change', function(evt) {
+  gulp.watch(config.paths.css.files, ['compile-sass'])
+    // When there is a change, display what file was changed, showing the path after the 'css' folder
+    .on('change', function (evt) {
       console.log(
         '[watcher] File ' + evt.path.replace(/.*(?=css)/,'') + ' was ' + evt.type + ', compiling...'
+        );
+      });
+
+  gulp.watch(config.paths.js.files, ['concat-minify-js'])
+    .on('change', function (evt) {
+      console.log(
+        '[watcher] File ' + evt.path.replace(/.*(?=js)/,'') + ' was ' + evt.type + ', compiling...'
         );
       });
 });
