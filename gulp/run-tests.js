123fadfd36bf37bf3e3a8a4b6ce793b5de0d9285
voothib <tvanspaandonck@DSK-BETV-027.voo.intra>
2014-08-05 11:20:10 +0200

    Gulp test tasks : scaffolding

---
+++ gulp/run-tests.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var gulp = require('gulp');
+var paths = require('./config').paths;
+
+gulp.task('run-tests', function () {
+  
+});
--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,14 @@ var gulp = require('gulp');
 require('./gulp/sass-compile');
 require('./gulp/js-minify');
 require('./gulp/js-hint');
+require('./gulp/run-tests');
 require('./gulp/watch');
 
 gulp.task('build', [
   'sass-compile',
   'js-minify',
-  'js-hint'
+  'js-hint',
+  'run-tests'
 ]);
 
 gulp.task('default', ['build'], function () {
