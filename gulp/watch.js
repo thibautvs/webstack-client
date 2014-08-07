b5339a5ab5a5fa18819bd834ae9febc3c2fe4186
voothib <tvanspaandonck@DSK-BETV-027.voo.intra>
2014-08-07 15:10:54 +0200

    Cleanup

--- gulp/watch.js
+++ gulp/watch.js
@@ -4,10 +4,8 @@ var gulp = require('gulp');
 var utils = require('./utils');
 var paths = require('./config').paths;
 
-// Watch the files in the paths object, and when there is a change, run the functions in the array
 gulp.task('watch', function () {
   gulp.watch(paths.css.files, ['sass-compile'])
-    // When there is a change, display what file was changed, showing the path after the 'css' folder
     .on('change', function (evt) {
       console.log('[watcher] File ' + utils.trimCssPath(evt.path) + ' was ' + evt.type + ', compiling...');
     });
