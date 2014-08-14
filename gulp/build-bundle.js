b8e4036cb2f42ead8b0fa3e7c88d5421e8ee40de
voothib <tvanspaandonck@DSK-BETV-027.voo.intra>
2014-08-14 11:25:34 +0200

    Implemented bundles fingerprinting + misc improvements

--- gulp/build-bundle.js
+++ gulp/build-bundle.js
@@ -1,20 +1,33 @@
 'use strict';
 
 var gulp = require('gulp');
-var fs = require('fs');
+var fsSync = require('fs-sync');
 var usemin = require('gulp-usemin');
 var byteDiff = require('gulp-bytediff');
 var minifyCss = require('gulp-minify-css');
 var ngAnnotate = require('gulp-ng-annotate');
 var uglify = require('gulp-uglify');
+var rev = require('gulp-rev');
 var utils = require('./utils');
 var paths = require('./config').paths;
 
 gulp.task('build-bundle', function () {
-  if (utils.isDevelopment) return;
+  fsSync.copy(paths.html.indexTemplate, paths.html.index, {force: true});
 
-  fs.createReadStream(paths.html.indexTemplate).pipe(
-    fs.createWriteStream(paths.html.index));
+  if (utils.isDevelopment) {
+    var warningMsg =
+      '<!-- !!! WARNING !!! -->'
+      + utils.endOfLine
+      + '<!-- AUTO GENERATED FILE. DO NOT EDIT, CHANGES WILL BE LOST ! -->'
+      + utils.endOfLine
+      + utils.endOfLine;
+
+    fsSync.write(
+      paths.html.index,
+      warningMsg + fsSync.read(paths.html.index) + warningMsg,
+      {encoding: 'utf8'});
+    return;
+  }
 
   return gulp.src(paths.html.index)
     .pipe(usemin({
@@ -22,15 +35,16 @@ gulp.task('build-bundle', function () {
         'concat',
         byteDiff.start(),
         minifyCss(),
-        byteDiff.stop()
+        byteDiff.stop(),
+        rev()
       ],
       js: [
         'concat',
         ngAnnotate(),
         byteDiff.start(),
         uglify(),
-        byteDiff.stop()
-        /*rev(),*/
+        byteDiff.stop(),
+        rev()
       ]
     }))
     .pipe(gulp.dest(paths.html.src));
